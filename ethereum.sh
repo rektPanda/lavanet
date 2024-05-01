e8479e6750c31ab68441dbe27ea9041258640459
Kenzhi <116340981+rektPanda@users.noreply.github.com>
2024-05-01 19:36:22 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-22cf1417-6d03-4352-89ec-49346ece0606/"
 
 OUTPUT_DIR="logs"
 
